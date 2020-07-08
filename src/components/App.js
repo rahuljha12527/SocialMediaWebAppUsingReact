38ab3213b02155ca9ba08c56bc9c692a4c2f320e
rahuljha661 <rahuljha12527@gmail.com>
2020-07-08 13:13:39 +0530

    add ui of post

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
+import {fetchPosts} from '../actions/posts';
 
-function App() {
-  return (
-    <div className="App">
-          App       
-       </div>
-  );
+  class App extends React.Component {
+
+      componentDidMount() {
+        this.props.dispatch(fetchPosts());
+      }
+      
+    render() {
+    
+    console.log('PROPS',this.props);
+    return     <div>App</div>
+    
+  }
 }
 
-export default App;
+
+function mapStateToProps(state){
+   return {
+     posts:state.posts
+   };
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
@@ -10,8 +11,10 @@ const store=configureStore();
 console.log('store',store.getState( ));
 
 ReactDOM.render(
-  <React.StrictMode>
+<Provider store={store}>
+<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
