cc07e3e81fa2190cbefc643f8649deec2410fcef
rahuljha661 <rahuljha12527@gmail.com>
2020-07-13 14:02:08 +0530

    adding the design of whole layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-
 import { fetchPosts } from '../actions/posts';
-import {PostsList} from './index';
+import { PostsList } from './index';
 
 class App extends React.Component {
   componentDidMount() {
@@ -16,7 +15,58 @@ class App extends React.Component {
 
     return (
       <div>
-       <PostsList posts={posts}/>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+                id="user-dp"
+              />
+            </div>
+            <div className="nav-links ">
+                  <ul>
+                    <li>Log in</li>
+                    <li>Log out</li>
+                    <li>Register</li>
+                  </ul>
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
       </div>
     );
   }
@@ -28,8 +78,8 @@ function mapStateToProps(state) {
   };
 }
 
-App.propTypes={ 
-  posts:PropTypes.array.isRequired
-}
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(App);
