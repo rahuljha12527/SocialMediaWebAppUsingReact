9af2878e90e175cf1bb15005785a7b0f5c8f47ab
rahuljha661 <rahuljha12527@gmail.com>
2020-07-14 12:54:50 +0530

    add login page

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ import App from '../components/App';
 import Navbar from './Navbar';
 import Home from './Home';
 import Page404 from './Page404';
-export { PostsList, App, Navbar,Home,Page404};
+import Login from './Login'; 
+
+export { PostsList, App, Navbar,Home,Page404,Login};
